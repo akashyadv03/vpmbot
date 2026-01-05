256af5ddfcad4297b5e1eb72d3f62d212d9d74aa
akashyadav2311 <rajuyarr80@gmail.com>
2026-01-06 02:13:47 +0530

    page not found page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export const dynamic = "force-static";
+
+export default function NotFound() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-2xl font-bold">404 – Page Not Found</h1>
+        </div>
+    );
+}
